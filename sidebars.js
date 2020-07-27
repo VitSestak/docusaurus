c7fa79edc058f08420721a20fd556fab56d717b7
Šesták, Vít <sestavit@fel.cvut.cz>
2020-07-27 11:51:35 +0200

    update

---
+++ sidebars.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "Docs": {
+        "Documents": [
+            'doc_intro',
+        {
+            "Java Micronaut Examples": ['micronaut/redis/redis', 'micronaut/postgre/postgre', 'micronaut/restApi/restapi'],
+        },
+        {
+            "Administration manuals": ['admin_manuals/creNewApp', 'admin_manuals/creNewCom', 'admin_manuals/depApp', 
+                            'admin_manuals/creNewEnv', 'admin_manuals/apiExeTes', 'admin_manuals/aleOve', 'admin_manuals/depMon'],
+        },
+        {
+            "Local development with CodeNOW": ['local_development/microLocDev/micLocDev', 'local_development/springLocDev/sprLocDev'],
+        },
+        {
+            "Java Spring Boot Complex Examples": ['spring/javaSpring/spring-boot'],
+        },
+        {
+            "Users administration": ['user_adm/invNewCol', 'user_adm/newUseReg', 'user_adm/sshSet'],
+        },
+        {
+            "Managed components administration": ['components/getNewApaKaf', 'components/getNewPos', 'components/getNewRed'],
+        }
+        ] 
+    }
+};
+
